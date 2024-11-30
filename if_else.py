4bad5ca26fcd25e2a4d169b72d59d417d4673f71
NikhilJamwal734 <jamwalnikhil734@gmail.com>
2024-11-30 15:02:15 +0530

    added

---
+++ if_else.py
@@ -0,0 +1,18 @@
+num = int(input("enetr the marks"))
+if(0<num<100):
+    if(num>=90):
+        print("A")
+    if(90>num>=80):
+        print("B")
+    if(80>num>=70):
+        print("c")
+    if(70>num>=60):
+        print("d")
+    if(num<60):
+        print("E")
+else:
+    print("invalid")
+
+
+
+
---
+++ libraries.py
@@ -0,0 +1,22 @@
+import math
+import random
+# import calendar 
+# from calendar import*
+from calendar import month,calendar 
+from calendar import month as m
+
+print(math.sqrt(16))
+print(math.sqrt(17))
+print(math.pow(12,2))
+print(math.factorial(5))
+
+print(random.random())
+print(random.randrange(1,5))
+
+# print(calendar.month(2022,5))
+# print(calendar.calendar(2025))
+
+# print(calendar(2020))
+# print(month(2022,6))
+
+print(m(2023,10))
